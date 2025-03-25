9fb41f8f56556fe0d9bfd0e5022b20acd078781a
BekjonCoder <k75154256@gmail.com>
2025-03-25 22:47:40 +0500

    dd

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 'use strict'
+const API='https://reqres.in/api/user'
 const userListDiv=document.getElementById('userList')
-
+const message=document.getElementById('message')
 
 let user=[]
 const createUser=async()=>{
-    const name=document.getElementById('name')
-    const job=document.getElementById('job')
-}
+    const name=document.getElementById('name').value
+    const job=document.getElementById('job').value
+
+
+    if(name && job){
+        message.textContent=`Iltimos to'+liq +ma+'lumot kriting`
+    }
+
+}   
