ac417f868693b128eecc29fcfafb1ea20f04772f
BekjonCoder <k75154256@gmail.com>
2025-03-25 22:38:07 +0500

    vvc

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 'use strict'
 const userListDiv=document.getElementById('userList')
+
+
+let user=[]
+const createUser=async()=>{
+    const name=document.getElementById('name')
+    const job=document.getElementById('job')
+}
